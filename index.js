6e582bb00fe0ba4c797497a3636d8eaea8a195bd
Osman Pérez <operez@edutecno.com>
2024-11-08 21:09:08 -0400

    Transacciones en node JS

--- index.js
+++ index.js
@@ -24,9 +24,45 @@ createServer((request, response) => {
 
         return request.on("end", async() => {
             body = JSON.parse(body);
-            const { rows, rowCount } = await conPool.query("SELECT * FROM actor LIMIT 5");
-            console.log({ rowCount, rows });
-            response.end(JSON.stringify({ message: "Asignando"}));
+
+            try {
+                await conPool.query("BEGIN");
+
+                // Consulta para validar si el actor o actriz existe   
+                const { rows: actors, rowCount: actorFind } = await conPool.query(
+                    "SELECT * FROM actor WHERE first_name ilike $1 AND last_name ilike $2",
+                    [ body.first_name, body.last_name]
+                );
+
+                let actor;
+                if(actorFind == 0) { // Si el actor o actriz no existe se registra.
+                    // Consulta para insertar actor o actriz
+                    let result = await conPool.query(
+                        "INSERT INTO actor(first_name, last_name) VALUES($1, $2) RETURNING *",
+                        [body.first_name, body.last_name]
+                    );
+                    actor = result.rows[0];
+                } else {
+                    actor = actors[0]
+                }
+
+                const { rows: films, rowCount: filmFind} = await conPool.query("SELECT * FROM film WHERE title ilike $1", [body.title]);
+                if(filmFind == 0) {
+                    response.writeHead(422);
+                    throw "Película no existe"
+                } else { // Escenario donde la película si existe.
+                    const result = await conPool.query(
+                        "INSERT INTO film_actor(film_id, actor_id) VALUES($1,$2)",
+                        [films[0].film_id, actor.actor_id]
+                    );
+                    await conPool.query("COMMIT");
+                    response.end(JSON.stringify({ message: "Actor asignado a la película de manera exitosa", actor, film: films[0]}));
+                }
+            } catch (error) {
+                await conPool.query("ROLLBACK");
+                response.writeHead(422);
+                response.end(JSON.stringify({ message: error.message || 'Error interno'}));
+            }   
         })
         
     }
