33d63ca58d5b3f3c05a87ff2d576b7c969fe61c3
Osman Pérez <operez@edutecno.com>
2024-11-15 21:15:48 -0400

    Asignación de categorías a películas

--- index.js
+++ index.js
@@ -67,6 +67,75 @@ createServer((request, response) => {
         
     }
 
+    if(request.method == "POST" && urlParsed.pathname == "/category/assign") {
+        let body = "";
+        request.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+
+        return request.on("end", async() => {
+            body = JSON.parse(body);
+
+            await conPool.query("BEGIN");
+            
+            try {
+                const resultCategory = await conPool.query("SELECT * FROM category WHERE name ilike $1", [body.category_name]);
+                if(resultCategory.rowCount == 0) {
+                    response.writeHead(404);
+                    return response.end(JSON.stringify({ message: "Categoría no existente"}));
+                }
+                const [category] = resultCategory.rows
+
+                const resultFilm = await conPool.query(
+                    "SELECT * FROM film WHERE title ilike $1 AND release_year=$2",
+                    [body.title, body.release_year]
+                )
+
+
+                if(resultFilm.rowCount == 0) {
+                    const insertFilm = await conPool.query(
+                        `INSERT INTO film (
+                                        title, description, release_year, language_id, rental_duration, 
+                                        rental_rate, length, replacement_cost, rating, last_update
+                        ) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, now()) RETURNING *;`, 
+                        [
+                            body.title, body.description, body.release_year, body.language_id, body.rental_duration,
+                            body.rental_rate, body.length, body.replacement_cost, body.rating
+                        ]
+                    );
+                    var [film] = insertFilm.rows
+                } else {
+                    const updateFilm = await conPool.query(
+                        `UPDATE film set title=$1, description=$2, release_year=$3, language_id=$4, 
+                                rental_duration=$5, rental_rate=$6, length=$7, replacement_cost=$8, 
+                                rating=$9, last_update=now()
+                        WHERE title=$1 AND release_year=$3
+                        RETURNING *;`, 
+                        [
+                            body.title, body.description, body.release_year, body.language_id, body.rental_duration,
+                            body.rental_rate, body.length, body.replacement_cost, body.rating
+                        ]
+                    );
+                    var [film] = updateFilm.rows
+                    console.log({ film });
+                }
+
+                const assignResult = await conPool.query(
+                    "INSERT INTO film_category(film_id, category_id, last_update) VALUES($1, $2, now())",
+                    [film.film_id, category.category_id]
+                )
+
+                await conPool.query("COMMIT");
+                return response.end(JSON.stringify({ message: "Categoría asignada a la película de manera exitosa"}));
+            } catch (error) {
+                await conPool.query("ROLLBACK");
+                return response.end(JSON.stringify({ message: error.message || "Error interno del servidor"}));
+            }
+           
+        });
+        
+    }
+
     response.writeHead(404);
     response.end(JSON.stringify({ message: "Ruta no encontrada"}));
 }).listen(3000, () => console.log("Servicio ejecutándose por el puerto 3000"))
